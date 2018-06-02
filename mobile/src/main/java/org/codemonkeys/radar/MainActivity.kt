2b67f62b2ba9d5fd270ea18e19798086f5e0dec1
Wim Hambrouck <wim.hambrouck@gmail.com>
2018-06-02 17:38:34 +0200

    Initial commit

---
+++ mobile/src/main/java/org/codemonkeys/radar/MainActivity.kt
@@ -0,0 +1,20 @@
+package org.codemonkeys.radar
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun createLocationRequest() {
+        val locationRequest = LocationRequest().apply {
+            interval = 10000
+            fastestInterval = 5000
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        }
+    }
+}
