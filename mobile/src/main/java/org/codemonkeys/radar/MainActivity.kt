5bdb91582931ffe3ac7c69bf4381d412c7886619
Wim Hambrouck <wim.hambrouck@gmail.com>
2018-06-02 18:00:01 +0200

    eerste location shizzle

--- mobile/src/main/java/org/codemonkeys/radar/MainActivity.kt
+++ mobile/src/main/java/org/codemonkeys/radar/MainActivity.kt
@@ -1,7 +1,12 @@
 package org.codemonkeys.radar
 
+import android.content.Intent
+import android.content.IntentSender
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.*
+import com.google.android.gms.tasks.Task
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,5 +21,37 @@ class MainActivity : AppCompatActivity() {
             fastestInterval = 5000
             priority = LocationRequest.PRIORITY_HIGH_ACCURACY
         }
+
+        val builder = LocationSettingsRequest.Builder()
+                .addLocationRequest(locationRequest)
+
+        val client: SettingsClient = LocationServices.getSettingsClient(this)
+        val task: Task<LocationSettingsResponse> = client.checkLocationSettings(builder.build())
+
+        task.addOnSuccessListener { locationSettingsResponse ->
+            // All location settings are satisfied. The client can initialize
+            // location requests here.
+            // ...
+        }
+
+        task.addOnFailureListener { exception ->
+            if (exception is ResolvableApiException){
+                // Location settings are not satisfied, but this can be fixed
+                // by showing the user a dialog.
+                try {
+                    // Show the dialog by calling startResolutionForResult(),
+                    // and check the result in onActivityResult().
+                    val REQUEST_CHECK_SETTINGS = 42
+                    exception.startResolutionForResult(this@MainActivity,
+                            REQUEST_CHECK_SETTINGS)
+                } catch (sendEx: IntentSender.SendIntentException) {
+                    // Ignore the error.
+                }
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+
     }
 }
